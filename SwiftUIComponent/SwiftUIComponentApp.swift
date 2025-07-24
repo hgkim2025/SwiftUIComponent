792b4552f0eb3982201954b44761adf94e669b2a
김현구 <hyeongu.kim@cubox.ai>
2025-07-24 10:45:57 +0900

    Initial Commit

---
+++ SwiftUIComponent/SwiftUIComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentApp.swift
+//  SwiftUIComponent
+//
+//  Created by 김현구 on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
